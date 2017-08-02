6485924d1fc7d90e1b98d69800dd6834c9ec4d2f
HyKurtis <hykurtisbusiness@gmail.com>
2017-08-02 02:33:46 +0100

    removed random stuff not gud nuf

--- src/main/java/UK/HYKURTIS/TERRIBLEJAVACODE/mAIN.java
+++ src/main/java/UK/HYKURTIS/TERRIBLEJAVACODE/mAIN.java
@@ -48,19 +48,11 @@ public class mAIN {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return "" + oNESnZeRoZ.frOmBizzPl0X(BINZ.substring(0)) + "";
 	}
 
 	public static lIFEmEANING geTTHIS(String THaT) {
 		return new lIFEmEANING();
 	}
-  
-		public static Object getTHATTTT()
-		{
-			
-        List<Object> randomThings = Arrays.asList(new String(), new Object(), new tERRIBLEcODEeXAMPLE(), "fuckin roulette bois");
-        return randomThings.get(new Random().nextInt(randomThings.size() - 1));
-			
-		}
 }
