a2534243f81a50a42c927a0bb89947e7e8d793d4
HyKurtis <hykurtisbusiness@gmail.com>
2017-08-02 01:18:24 +0100

    .

--- src/main/java/UK/HYKURTIS/TERRIBLEJAVACODE/mAIN.java
+++ src/main/java/UK/HYKURTIS/TERRIBLEJAVACODE/mAIN.java
@@ -4,6 +4,6 @@ public class mAIN {
 	public static tERRIBLEcODEeXAMPLE TERRIBLECODEEXAMPLE;
 
 	public static void main(String[] args) {
-		TERRIBLECODEEXAMPLE = new tERRIBLEcODEeXAMPLE();
+		mAIN.TERRIBLECODEEXAMPLE = new tERRIBLEcODEeXAMPLE();
 	}
 }
